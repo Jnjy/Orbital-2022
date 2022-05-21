3596d25b3d59124f22b825b3a83444fba0eb1caa
jnjy <jefferyng2000@gmail.com>
2022-05-21 16:49:01 +0800

    Added Plugins

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA51xTa_HDa6q1gUUygM6jbChTCBRn8idM",
+    authDomain: "orbital-2022-95df4.firebaseapp.com",
+    projectId: "orbital-2022-95df4",
+    storageBucket: "orbital-2022-95df4.appspot.com",
+    messagingSenderId: "461288548077",
+    appId: "1:461288548077:web:84d86d9266a8cc1d958ee4",
+    measurementId: "G-T3LN0X7G8E"
+  };
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+function LoginPage() {
+  return (
+    <Grid
+    container
+    direction="column"
+    justifyContent="center"
+    alignItems="center"
+    >
+    
+    </Grid>
+  )
+}
+
+export default LoginPage
