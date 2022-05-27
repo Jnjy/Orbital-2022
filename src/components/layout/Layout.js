4c8f50d800c1a1721bcbc36a7892681ccc68496b
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-05-28 01:02:30 +0800

    Fix layout for header, sidebar and content

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,19 @@
 import classes from "./Layout.module.css";
 import Sidebar from "./Sidebar";
+import Appbar from "./Appbar";
 
 function Layout(props) {
   return (
-    <div>
-      <Sidebar />
+    <div className={classes.wrapper}>
+      <div className={classes.TopBar}>
+        <Appbar />
+      </div>
+      <div className={classes.content}>
+        <div>
+          <Sidebar />
+        </div>
+        <main>{props.children}</main>
+      </div>
     </div>
   );
 }
--- src/pages/community/CommunityPage.js
+++ src/pages/community/CommunityPage.js
@@ -1,7 +1,12 @@
 import Layout from "../../components/layout/Layout.js";
+import classes from "./CommunityPage.module.css";
 
 function CommunityPage() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div className={classes.contentmain}>Hello!</div>
+    </Layout>
+  );
 }
 
 export default CommunityPage;
