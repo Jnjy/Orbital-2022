33cfaeba0abb08d563020eb8ea121ad976927770
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-05-28 11:45:19 +0800

    Fixed sidebar & Topbar scaling issue

--- src/pages/community/CommunityPage.js
+++ src/pages/community/CommunityPage.js
@@ -9,111 +9,31 @@ function CommunityPage() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
           convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
           vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
+          placerat volutpat condimentum diam.
         </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
           convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
           vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
+          placerat volutpat condimentum diam.
         </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
           convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
           vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
+          placerat volutpat condimentum diam.
         </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
           convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
           vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
+          placerat volutpat condimentum diam.
         </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
           convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
           vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
-          vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
-          vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
-          vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
-          vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
-          vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          convallis, nunc et elementum pretium, turpis libero feugiat nulla, sed
-          vulputate neque augue ut metus. Vivamus vel magna in lorem varius
-          placerat volutpat condimentum diam. Nunc congue feugiat mollis. Aenean
-          interdum mauris porta auctor pulvinar. Aliquam efficitur elit sed
-          libero pharetra, eu blandit libero faucibus. Vestibulum maximus lectus
-          non nisi ultricies aliquet. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas.
+          placerat volutpat condimentum diam.
         </p>
       </div>
     </Layout>
