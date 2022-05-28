bd5efa1324ffedf20fd3e4465b51cbdaab09e365
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-05-28 20:28:12 +0800

    Extract name and display on appbar

--- src/App.js
+++ src/App.js
@@ -8,22 +8,24 @@ import ResetPasswordPage from "./pages/resetPassword/ResetPasswordPage";
 import CommunityPage from "./pages/community/CommunityPage";
 import UserProfile from "./pages/profile/UserProfile";
 
-
 function App() {
   const { user } = useAuth();
 
   // rabak way need to change
   return (
     <div className="App">
-      {user ? <CommunityPage /> 
-      : (<Routes>  
-          <Route index element={user ? <CommunityPage /> : <LoginPage />} /> 
+      {user ? (
+        <CommunityPage />
+      ) : (
+        <Routes>
+          <Route index element={user ? <CommunityPage /> : <LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/community" element={<CommunityPage />} />
-        </Routes>)}
+        </Routes>
+      )}
     </div>
   );
 }
--- src/components/layout/Appbar.js
+++ src/components/layout/Appbar.js
@@ -1,16 +1,31 @@
 import classes from "./Appbar.module.css";
 import Avatar from "@mui/material/Avatar";
+import { useState, useEffect } from "react";
+
+import { useAuth } from "../../hooks/useAuth";
 
 //Appbar is also the top bar icymi
 
 function Appbar(props) {
+  const { user } = useAuth();
+
+  const [name, setName] = useState("Loading name...");
+
+  useEffect(() => {
+    if (user?.displayName) {
+      setName(user.displayName);
+    }
+  }, [user]);
+
+  console.log(name);
+
   return (
     <div className={classes.top}>
       <div className={classes.pagename}>Pagename</div>
       <div className={classes.user}>
-        <div className={classes.username}>Jason</div>
+        <div className={classes.username}>{name}</div>
         <div className={classes.avatar}>
-          <Avatar>J</Avatar>
+          <Avatar>{name.charAt(0)}</Avatar>
         </div>
       </div>
     </div>
