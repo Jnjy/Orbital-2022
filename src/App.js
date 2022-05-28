ba2a5ce6824b17d6ffc970cdbb68e611f0aed758
jnjy <jefferyng2000@gmail.com>
2022-05-28 08:49:21 +0800

    [Edit] Minor Adjustment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { useAuth } from './hooks/useAuth';
+import "./App.css";
+import { useAuth } from "./hooks/useAuth";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignUpPage from './pages/signup';
-import LoginPage from './pages/login/LoginPage';
-import ResetPasswordPage from './pages/resetPassword/ResetPasswordPage';
-import UserProfile from './pages/profile/UserProfile';
+import SignUpPage from "./pages/signup";
+import LoginPage from "./pages/login/LoginPage";
+import ResetPasswordPage from "./pages/resetPassword/ResetPasswordPage";
+import UserProfile from "./pages/profile/UserProfile";
 
 function App() {
   const { user } = useAuth();
@@ -12,12 +12,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={user ? <LoginPage /> : <SignUpPage />} /> 
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/signup" element={<SignUpPage />} />
-          <Route exact path="/reset-password" element={<ResetPasswordPage />} />
-          <Route exact path="/profile" element={<UserProfile />} />
+        <Routes>  
+          <Route path="/" element={user ? <LoginPage /> : <SignUpPage />} /> 
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Routes>
       </BrowserRouter>
     </div>
