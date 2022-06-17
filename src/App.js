1a4788e3962de73419139479b442e058524b6564
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-06-17 14:57:37 +0800

    Fix spacings

--- src/App.js
+++ src/App.js
@@ -15,15 +15,15 @@ function App() {
   // rabak way need to change
   return (
     <div className="App">
-        <Routes>
-          <Route index element={user ? <CommunityPage /> : <LoginPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/community" element={<CommunityPage />} />
-          <Route path="/store" element={<StorePage />} />
-        </Routes> 
+      <Routes>
+        <Route index element={user ? <CommunityPage /> : <LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/store" element={<StorePage />} />
+      </Routes>
     </div>
   );
 }
