a9672b10de5285132f09dc80a32a2f0b921b3acb
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-06-18 22:22:12 +0800

    Add document creation during singup

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,15 @@
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
-import { initializeApp } from 'firebase/app';
-import { app, firebaseConfig } from '../config/FirebaseConfig';
-import { getFirestore } from '@firebase/firestore'
-import { collection, getDocs } from 'firebase/firestore'
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { app, firebaseConfig, db } from "../config/FirebaseConfig";
+import { getFirestore, doc, setDoc } from "@firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import React, { useState, useEffect, useContext, createContext } from "react";
 
 const firebaseAuth = getAuth(app);
@@ -10,6 +17,12 @@ const firebaseAuth = getAuth(app);
 const googleAuthProvider = new GoogleAuthProvider();
 
 const authContext = createContext();
+
+const usersRef = collection(db, "users");
+
+const createUser = async (id, data) => {
+  await setDoc(doc(db, "users", id), data);
+};
 
 export function ProvideAuth({ children }) {
   const auth = useProvideAuth();
@@ -21,54 +34,62 @@ export const useAuth = () => {
 
 function useProvideAuth() {
   const [user, setUser] = useState(null);
-  //const usersCollectionRef = collection(db, "users");
 
   const signin = (email, password) => {
     return signInWithEmailAndPassword(firebaseAuth, email, password)
-    .then((userCredential) => {
-      const newUser = userCredential.user;
-      setUser(newUser);
-    })
-    .catch((error) => {
-      throw error;
-    });
+      .then((userCredential) => {
+        const newUser = userCredential.user;
+        setUser(newUser);
+      })
+      .catch((error) => {
+        throw error;
+      });
   };
 
   const signup = (email, password) => {
-    return createUserWithEmailAndPassword(firebaseAuth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const newUser = userCredential.user;
-      setUser(newUser)
-    })
-    .catch((error) => {
-      throw error;
-    });
+    return (
+      createUserWithEmailAndPassword(firebaseAuth, email, password)
+        //.then((userCredential) => {console.log(userCredential.user.uid);return userCredential;})
+        //.then((userCredential) => {usersRef.doc(`${userCredential.user.uid}`).set({name: "nameplaceholder",uid: userCredential.user.uid,});console.log("Doc created");return userCredential;})
+        .then((userCredential) => {
+          createUser(userCredential.user.uid, { name: "nameplaceholder" });
+          //DEBUG: for debugging use
+          console.log(userCredential.user.uid);
+          console.log("Reeturning");
+          return userCredential;
+        })
+        .then((userCredential) => {
+          //Signed in
+          const newUser = userCredential.user;
+          setUser(newUser);
+        })
+        .catch((error) => {
+          throw error;
+        })
+    );
   };
 
   const signout = () => {
-    return signOut(firebaseAuth).then(() => {
-      console.log("Sign Out Successful");
-      setUser(null);
-    }).catch((error) => {
-      // An error happened.
-    });
+    return signOut(firebaseAuth)
+      .then(() => {
+        console.log("Sign Out Successful");
+        setUser(null);
+      })
+      .catch((error) => {
+        // An error happened.
+      });
   };
 
   const sendPasswordResetEmail = (email) => {
-    return firebaseAuth 
-      .sendPasswordResetEmail(email)
-      .then(() => {
-        return true;
-      });
+    return firebaseAuth.sendPasswordResetEmail(email).then(() => {
+      return true;
+    });
   };
 
   const confirmPasswordReset = (code, password) => {
-    return firebaseAuth
-      .confirmPasswordReset(code, password)
-      .then(() => {
-        return true;
-      });
+    return firebaseAuth.confirmPasswordReset(code, password).then(() => {
+      return true;
+    });
   };
 
   const signInWithGoogle = () => {
@@ -80,7 +101,7 @@ function useProvideAuth() {
     //  const data = await getDocs(usersCollectionRef);
     //  console.log(data.docs);
     //};
-    
+
     const unsubscribe = firebaseAuth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
@@ -100,6 +121,6 @@ function useProvideAuth() {
     signout,
     sendPasswordResetEmail,
     confirmPasswordReset,
-    signInWithGoogle
+    signInWithGoogle,
   };
 }
