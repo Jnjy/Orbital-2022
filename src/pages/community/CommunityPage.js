552bc942a8789e6589da2053002de74a74fcabf6
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-06-22 00:15:17 +0800

    Fix querying DB for user's comm info

--- src/pages/community/CommunityPage.js
+++ src/pages/community/CommunityPage.js
@@ -9,15 +9,15 @@ import { getCommunityInfo, queryCommunity } from "../../hooks/useDB.js";
 function CommunityPage(props) {
   const [commList, setCommList] = useState([]);
   const [allCommInfo, setAllCommInfo] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { user } = useAuth();
 
-  let comms = [];
-
   useEffect(() => {
     if (user) {
-      queryCommunity(user.uid).then((res) => {
+      //Putting a placeholder here until adding user to community is setup
+      //queryCommunity(user.uid).then((res) => {
+      queryCommunity("e9iLoiJsNUTd0mGvBUKAjexLHOl1").then((res) => {
         setCommList(res);
         setIsLoading(true);
       });
@@ -25,13 +25,15 @@ function CommunityPage(props) {
   }, [user]);
 
   useEffect(() => {
+    let comms = [];
     commList.forEach((r) => {
-      getCommunityInfo(r)
-        .then((res) => {
-          comms.push(res);
-        })
-        .then(setAllCommInfo(comms), setIsLoading(false));
+      getCommunityInfo(r).then((res) => {
+        comms.push(res);
+        console.log(comms.length);
+        setAllCommInfo(comms);
+      });
     });
+    setIsLoading(false);
   }, [commList]);
 
   return (
@@ -47,16 +49,12 @@ function CommunityPage(props) {
       >
         {/* add search and filter bar
         to be replaced by mapping from db*/}
+        {allCommInfo.map(({ name, shortDesc }) => (
+          <CommCard title={name} desc={shortDesc} />
+        ))}
+        {/* {console.log(allCommInfo[0])} */}
         <CommCard
-          title="Community 1"
-          desc="This is a short description of community 1"
-        />
-        <CommCard
-          title="Community 1"
-          desc="This is a short description of community 1"
-        />
-        <CommCard
-          title="Community 1"
+          title="Placeholder 1"
           desc="This is a short description of community 1"
         />
       </Grid>
