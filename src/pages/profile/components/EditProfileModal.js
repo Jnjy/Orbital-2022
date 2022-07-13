42cf49827e70784d076aa03f6749ac65c7c39400
jnjy <jefferyng2000@gmail.com>
2022-07-13 15:17:52 +0800

    feat: added profile edit modal

---
+++ src/pages/profile/components/EditProfileModal.js
@@ -0,0 +1,42 @@
+import { Button, Modal, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import EditProfileForm from "./EditProfileForm";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  bgcolor: "white",
+  boxShadow: 24,
+  p: 4,
+};
+
+function CommunityModal(props) {
+    
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        console.log(props)
+        setOpen(true);
+    }
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+        <Button variant="outlined" onClick={handleOpen}>Edit Profile</Button>
+        <Modal open={open} onClose={handleClose} closetimeoutms={1000}>
+            <Box sx={modalStyle}>
+            <Stack spacing={2}>
+                <Typography variant="h5">Edit Profile</Typography>
+                <br />
+                <EditProfileForm props={props}/>
+            </Stack>
+            </Box>
+        </Modal>
+        </div>
+    );
+}
+
+export default CommunityModal;
