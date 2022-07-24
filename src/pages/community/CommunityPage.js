9c6f200916769c59a9e4ae69ce9129d47faaf33c
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-07-24 18:46:51 +0800

    Remove unnecessary console log from CommuityPage

--- src/pages/community/CommunityPage.js
+++ src/pages/community/CommunityPage.js
@@ -41,7 +41,7 @@ function CommunityPage(props) {
         tmp[i] = r[i][2];
       }
       Promise.all(tmp).then((res) => {
-        console.log(res);
+        //console.log(res);
         for (let j = 0; j < r.length; j++) {
           r[j][2] = res[j];
         }
